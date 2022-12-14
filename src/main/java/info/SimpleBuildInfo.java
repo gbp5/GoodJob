11941e5f7d41555b7779e97f83bf4c9b32a43655
gbp5 <balu.grandhi5@gmail.com>
2022-12-14 11:34:05 +0530

    Added pom file

---
+++ src/main/java/info/SimpleBuildInfo.java
@@ -0,0 +1,18 @@
+package info;
+
+import java.time.LocalDateTime;
+
+public class SimpleBuildInfo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Loading required Info :: ");
+		
+		String javaVersion = Runtime.version().toString();
+		String time = LocalDateTime.now().toString();
+		System.out.println(String.format("********\n Build Time: %s \n Java Version: %s \n********", time, javaVersion));
+
+	}
+
+}
